8d0ddbedba9c1456d3eb41e66eb80fd9a75d7dda
ND Glaus <103729772+NDG-Remote@users.noreply.github.com>
2024-07-23 15:30:03 +0200

    running, before editing

--- app.config.js
+++ app.config.js
@@ -1,48 +1,44 @@
-const IS_DEV = process.env.APP_VARIANT === 'development';
-
+const IS_DEV = process.env.APP_VARIANT === "development";
 
 export default {
-  "expo": {
-    "name": "changemakers-rn",
-    "slug": "changemakers-rn",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "userInterfaceStyle": "light",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+  expo: {
+    name: "changemakers-rn",
+    slug: "changemakers-rn",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
     },
-    "scheme": "io.frappe.changemakers",
-    "assetBundlePatterns": [
-      "**/*"
-    ],
-    "ios": {
-      "supportsTablet": true,
-      "config": {
-        "usesNonExemptEncryption": false
+    scheme: "io.frappe.mobileappphamos",
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+      config: {
+        usesNonExemptEncryption: false,
       },
-      "bundleIdentifier": IS_DEV ? 'io.changemakers.dev' : 'io.frappe.changemakers',
+      bundleIdentifier: IS_DEV
+        ? "io.changemakers.dev"
+        : "io.frappe.mobileappphamos",
     },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
       },
-      "package": "io.frappe.changemakers"
+      package: "io.frappe.mobileappphamos",
     },
-    "web": {
-      "favicon": "./assets/favicon.png"
+    web: {
+      favicon: "./assets/favicon.png",
     },
-    "extra": {
-      "eas": {
-        "projectId": "8fdd6952-bd1f-4c65-90fb-3a696ad52c97"
-      }
+    extra: {
+      eas: {
+        projectId: "8fdd6952-bd1f-4c65-90fb-3a696ad52c97",
+      },
     },
-    "plugins": [
-      "expo-build-properties",
-      "expo-secure-store"
-    ]
-  }
-}
+    plugins: ["expo-build-properties", "expo-secure-store"],
+  },
+};
--- src/data/constants.js
+++ src/data/constants.js
@@ -1,7 +1,7 @@
-export const BASE_URI = "https://platform.buildwithhussain.dev";
+export const BASE_URI = "https://phamos15dev.frappe.cloud";
 
 // must match the "scheme" property of the "app.json" file
-export const REDIRECT_URL_SCHEME = "io.frappe.changemakers"; 
+export const REDIRECT_URL_SCHEME = "io.frappe.mobileappphamos";
 
 export const SECURE_AUTH_STATE_KEY = "AuthState";
-export const OAUTH_CLIENT_ID = "68eaa32afd";
+export const OAUTH_CLIENT_ID = "m7br54jt0f";
